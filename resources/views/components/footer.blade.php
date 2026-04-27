dd4da78db0bc69d1674fa1e5c35ae693fd91e593
daniiii1222 <danielarecalde999@gmail.com>
2026-04-27 12:56:22 -0300

    footer mod

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,40 +1,46 @@
-<footer class="mi-footer mt-5 texto ">
-      <div class="container text-center text-md-start py-4">
-        <div class="row">
-          
-          <!-- Info del proyecto -->
-          <div class="col-md-4 mb-3">
-            <h5 class="footer-title">PetShop del Litoral</h5>
-            <p class="footer-text">
-              Tu tienda de confianza para productos de mascotas 🐾
-            </p>
-          </div>
+<footer class="footer">
 
-          <!-- Links útiles -->
-          <div class="col-md-4 mb-3">
-            <h5 class="footer-title">Enlaces</h5>
-            <ul class="list-unstyled footer-text">
-              <li><a href="{{ url('/') }}">Inicio</a></li>
+  <div class="footer-wave-top"></div>
+
+  <div class="footer-container">
+
+    <!-- Marca -->
+    <div class="footer-col">
+      <div class="icon-circle">
+        <i class="bi bi-heart-fill"></i>
+      </div>
+      <h3>PetShop del Litoral</h3>
+      <p>Tu tienda de confianza para productos de mascotas</p>
+    </div>
+
+    <!-- Enlaces -->
+    <div class="footer-col">
+      <div class="icon-circle">
+        <i class="bi bi-link-45deg"></i>
+      </div>
+      <h4>Enlaces</h4>
+      <ul>
+         <li><a href="{{ url('/') }}">Inicio</a></li>
               <li><a href="{{ url('/productos') }}">Productos</a></li>
               <li><a href="{{ url('/contacto') }}">Contacto</a></li>
               <li><a href="{{ url('/terminosYUsos') }}"> Términos y Usos</a></li>
-            </ul>
-          </div>
+      </ul>
+    </div>
 
-          <!-- Integrantes -->
-          <div class="col-md-4 mb-3 footer-text">
-            <h5 class="footer-title">Integrantes</h5>
-            <ul class="list-unstyled">
-              <li>Daniela Recalde</li>
-              <li>Virginia Romero</li>
-            </ul>
-          </div>
-
-        </div>
-
-        <!-- Línea inferior -->
-        <div class="text-center pt-3 border-top mt-3 footer-text">
-          <small>© 2026 PetShop del Litoral - Todos los derechos reservados</small>
-        </div>
+    <!-- Integrantes -->
+    <div class="footer-col">
+      <div class="icon-circle">
+        <i class="bi bi-people-fill"></i>
       </div>
+      <h4>Integrantes</h4>
+      <p><i class="bi bi-paw-fill"></i> Daniela Recalde</p>
+      <p><i class="bi bi-paw-fill"></i> Virginia Romero</p>
+    </div>
+
+  </div>
+
+  <div class="footer-bottom">
+    © 2026 PetShop del Litoral - Todos los derechos reservados
+  </div>
+
 </footer>
